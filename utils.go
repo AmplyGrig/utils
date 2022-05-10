0029c85fd067e73eb8857876d6e8b0559c53825a
Andrew Grigoriev <andrcop@gmail.com>
2022-05-10 20:28:54 +0300

    Change func name

--- utils.go
+++ utils.go
@@ -10,8 +10,8 @@ func InSlice(a []string, x string) bool {
 	return false
 }
 
-//ContainsInt check if int is in []int
-func ContainsInt(a []int, x int) bool {
+//InSliceInt check if int is in []int
+func InSliceInt(a []int, x int) bool {
 	for _, n := range a {
 		if x == n {
 			return true
